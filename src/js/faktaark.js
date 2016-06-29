e6b7543dc0c11258b3775ce3057f4e6d8f0a0f2f
Håvard Vidme <havard.vidme@gmail.com>
2016-06-29 08:35:21 +0200

    Restructured layout
    Major changes to structure, styles and scripts.

--- src/js/faktaark.js
+++ src/js/faktaark.js
@@ -1,38 +1,54 @@
-var fooHandler = function (event) {
-    var tbody = document.getElementById('bar');
-    var rows = tbody.getElementsByTagName('tr');
-    tbody.appendChild(rows[0]);
-    //console.log(rows);
+String.prototype.firstUpperCase = function () {
+    return this.substr(0, 1).toUpperCase() + this.substr(1);
 }
-document.getElementById('foo').addEventListener('click', fooHandler, false);
 
-var order = [4, 1, 3, 2];var rekkefølge = [];
-var foobarHandler = function (event) {
-    var tbody = document.getElementById('bar');
-    for (var idx in order) {
-        tbody.appendChild(document.getElementById('ssr-row-'+order[idx].toString()));
-        console.log(order[idx]);
-    }
-    /*var rows = tbody.getElementsByTagName('tr');
-    tbody.appendChild(rows[0]);*/
-    //console.log(rows);
-}
-document.getElementById('foobar').addEventListener('click', foobarHandler, false);
+var i,j;
 
 var translations = {
+    'nor': 'Norsk',
+    'sme': 'Nordsamisk',
     'nevneform': 'Nemneform',
     'nominativ': 'Nominativ',
     'genitiv-akkusativ': 'Genetiv akkusativ',
     'iverksattVedtak': 'Vedtak iverksatt',
+    'ubehandlet': 'Ubehandla',
+    'vedtatt': 'Vedtak',
+    'godkjent': 'Godkent',
+    'avslått': 'Avslege',
+    'feilført': 'Feilført',
     'hovednavn': 'Hovudnamn',
-    'sidenavn': 'Sidenamn'
+    'sidenavn': 'Sidenamn',
+    'bruk': 'Bruk',
+    'gardsbebyggelse': 'Gardsbebyggelse',
+    'bebyggelse': 'Bebyggelse',
+    'kommune': 'Kommune',
+    'administrativeIndelinger': 'Administrative inndelinger',
+    'offentligAdministrasjon': 'Offentlig administrasjon'
 };
 
-var transatables = document.getElementsByClassName('translate');
-for (var i = 0, j = transatables.length; i < j; i++) {
-    var content = (transatables[i].innerText || transatables[i].textContent).trim();
+var translates = document.getElementsByClassName('translate');
+for (i = 0, j = translates.length; i < j; i++) {
+    var content = (translates[i].innerText || translates[i].textContent).trim();
+    console.log(content);
     if (translations.hasOwnProperty(content)) {
-        transatables[i].textContent = translations[content];
-        transatables[i].classList.add('translated');
+        translates[i].textContent = translations[content];
+        translates[i].classList.add('translated');
+    } else {
+        translates[i].textContent = content.firstUpperCase();
     }
+}
+
+var spellingClickHandler = function (event) {
+    event.preventDefault();
+    var targetId = this.dataset.targetId || '';
+    if  (targetId != '') {
+        var targetElement = document.getElementById(targetId);
+        if (targetElement !== null) {
+            targetElement.classList.toggle('open');
+        }
+    }
+}
+var spellings = document.getElementsByClassName('js-spelling-toggle');
+for (i = 0, j = spellings.length; i < j; i++) {
+    spellings[i].addEventListener('click', spellingClickHandler, false);
 }
